7413992914c94c3e2926375627da2d9401353999
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 22:04:21 +0900

    店舗情報の複数設定方法

--- spec/requests/shops_spec.rb
+++ spec/requests/shops_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Shops", type: :request do
+  let!(:shops) { create_list(:shop, 5) }
   describe "GET /shops" do
     it "return status 200" do
       get shops_path
