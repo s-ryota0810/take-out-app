648e435e9dbfce72fb5b5251c66f0b634b2fb93b
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 20:14:48 +0900

    new,createアクションの追加

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,4 +4,24 @@ class ShopsController < ApplicationController
     @shops = Shop.all
   end
 
+  def new
+    @shop = Shop.new
+  end
+  
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to root_path, notice: '店舗情報を登録しました'
+    else
+      flash.now[:error] = '登録できませんでした'
+      render :new
+    end
+  end
+  
+  private
+  
+  def shop_params
+    params.require(:shop).permit(:name)
+  end
+
 end
