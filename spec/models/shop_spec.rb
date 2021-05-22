a47a5dee1cbabf92f05dd12d7f680717c6ff41b2
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 21:31:07 +0900

    店舗名が入力されていない時のテストを作成

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe Shop, type: :model do
     let!(:shop) do
       Shop.new(
         name: 'test'
-      )      
+      )
     end
 
     it 'shop info can be saved' do
       expect(shop).to be_valid
+    end
+  end
+  
+  context 'If a store name is not entered' do
+    let!(:shop) do
+      Shop.create
+    end
+
+    it 'shop info can not be saved' do
+      expect(shop.errors.messages[:name][0]).to eq("can't be blank")
     end
   end
 end
