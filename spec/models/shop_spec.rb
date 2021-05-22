c6978e4032cca663c6c98a0a9724c3842a34b45a
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 21:15:40 +0900

    店舗情報の登録についてテスト

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  
+  it 'shop info can be saved if shop name is entered' do
+    shop = Shop.new(
+      name: 'test'
+    )
+    
+    expect(shop).to be_valid
+  end
 end
