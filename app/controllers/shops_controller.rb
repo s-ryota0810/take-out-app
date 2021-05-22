a0c89129193bc6a66e9e8434cbd27a5af7350b6e
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 20:24:04 +0900

    showアクションの追加

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -11,13 +11,17 @@ class ShopsController < ApplicationController
   def create
     @shop = Shop.new(shop_params)
     if @shop.save
-      redirect_to root_path, notice: '店舗情報を登録しました'
+      redirect_to @shop, notice: '店舗情報を登録しました'
     else
       flash.now[:error] = '登録できませんでした'
       render :new
     end
   end
   
+  def show
+    @shop = Shop.find(params[:id])
+  end
+
   private
   
   def shop_params
