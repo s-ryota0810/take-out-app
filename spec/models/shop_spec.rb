7c4664728a898ec97ce92507f0d18a80b7c02d4e
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 21:26:17 +0900

    letを用いた変数定義でインスタンス変数を除去する

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
   context 'If a store name is entered' do
-    before do
-      @shop = Shop.new(
+    let!(:shop) do
+      Shop.new(
         name: 'test'
-      )
+      )      
     end
 
     it 'shop info can be saved' do
-      expect(@shop).to be_valid
+      expect(shop).to be_valid
     end
   end
 end
