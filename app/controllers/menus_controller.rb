c1fcda6ab2f501833b051cd47d6f37de9a1d94d9
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-22 22:58:02 +0900

    new,createアクションの追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,24 @@
 class MenusController < ApplicationController
+  
+  def new
+    @shop = Shop.find(params[:shop_id])
+    @menu = @shop.menus.build
+  end
+  
+  def create
+    shop = Shop.find(params[:shop_id])
+    @menu = shop.menus.create(menu_params)
+    if @menu.save
+      redirect_to shop_path(shop)
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def menu_params
+    params.require(:menu).permit(:name, :price)
+  end
+
 end
