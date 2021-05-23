5433ec3965d5776f71a78fd91d4b3c2c27cfa916
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-23 21:33:30 +0900

    new,createアクションの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,23 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   
+  def new
+    @shop = Shop.find(params[:shop_id])
+    @comment = current_user.comments.build
+  end
+  
+  def create
+    @comment = current_user.comments.create(comment_params)
+    if @comment.save
+      redirect_to shop_path(@comment.shop)
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:title, :content, :star).merge(params.permit(:shop_id))
+  end
 end
