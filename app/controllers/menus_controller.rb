053887255c524bedf3c67f99bac0354560773bc9
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-23 13:56:23 +0900

    edit,updateアクションの追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -15,6 +15,21 @@ class MenusController < ApplicationController
     end
   end
   
+  def edit
+    @shop = Shop.find(params[:shop_id])
+    @menu = @shop.menus.find(params[:id])
+  end
+  
+  def update
+    shop = Shop.find(params[:shop_id])
+    @menu = shop.menus.find(params[:id])
+    if @menu.update(menu_params)
+      redirect_to shop_path(shop)
+    else
+      render :edit
+    end
+  end
+  
   private
   
   def menu_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root to: 'shops#index'
 
   resources :shops do
-    resource :menus
+    resources :menus
   end
   
 end
