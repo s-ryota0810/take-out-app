816314850624f61b997682994b0fe75c5c89bd59
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-24 00:27:29 +0900

    Comment System テストの追加

---
+++ spec/system/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Comment', type: :system do
+  let!(:user) { create(:user) }
+  let!(:shop) { create(:shop) }
+  let!(:comments) { create_list(:comment, 3, shop: shop, user: user) }
+   
+  it 'comment list can be displayed' do
+    visit shop_path(shop)
+    comments.each do |comment|
+      expect(page).to have_css('.show_comment_title', text: comment.title)
+    end
+  end
+end
