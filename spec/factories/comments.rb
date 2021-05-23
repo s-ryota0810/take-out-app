3b9251e06c7e0a0d0ac183a79d280c524f7e66ec
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-23 23:47:50 +0900

    Comment Modelテストの追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    title { Faker::Lorem.characters(number: 3) }
+    content { Faker::Lorem.characters(number: 100) }
+    star { Faker::Number.between(from:1, to: 5) }    
   end
 end
