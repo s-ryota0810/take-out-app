48edb9a0da7a71a9aad89d596bdc350cbe234abc
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-24 21:45:47 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
