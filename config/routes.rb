5b127c45434654fd72035fd218dac80f14904344
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-24 20:37:04 +0900

    guest_sign_inアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'shops#index'
 
