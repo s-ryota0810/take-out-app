192c17e83b82ec54c221c55319453fbebf6c58ab
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-06 16:03:24 +0900

    リファクタリング（ファイル分割、スタイル）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,59 +7,9 @@ import Rails from '@rails/ujs'
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from '@rails/ujs'
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
 require('packs/raty')
 
 Rails.start()
 ActiveStorage.start()
 
 window.$ = window.jQuery = require('jquery');
-
-const handleFavoriteDisplay = (hasFavorited) => {
-  if (hasFavorited) {
-    $('.with_favorited').removeClass('hidden')
-  } else {
-    $('.non_favorited').removeClass('hidden')
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const dataset = $('#shop_show').data()
-  const shopId = dataset.shopId
-  
-  axios.get(`/shops/${shopId}/favorites`)
-    .then((response) => {
-      const hasFavorited = response.data.hasFavorited
-      handleFavoriteDisplay(hasFavorited)
-    })
-  
-  $('.non_favorited').on('click', () =>{
-    axios.post(`/shops/${shopId}/favorites`)
-      .then((response) => {
-        console.log(response)
-      })
-      
-      .catch((error) => {
-        window.alert('Error')
-        console.log(error)
-      })
-  })
-  
-  $('.with_favorited').on('click', () =>{
-    axios.delete(`/shops/${shopId}/favorites`)
-      .then((response) => {
-        console.log(response)
-      })
-      
-      .catch((error) => {
-        window.alert('Error')
-        console.log(error)
-      })
-  })
-
-})
