45224cb3c941d492ea295790619cd4d2eef42ae0
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-20 23:58:02 +0900

    dockerfile修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.reload_classes_only_on_change = false
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
